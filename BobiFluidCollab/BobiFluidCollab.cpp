42b981c3c5451542a0cb70530e79ab4d4f0f6b69
Stefan Monov <logixoul@gmail.com>
2024-12-29 12:25:46 +0200

    Add project files.

---
+++ BobiFluidCollab/BobiFluidCollab.cpp
@@ -0,0 +1,12 @@
+﻿// BobiFluidCollab.cpp : Defines the entry point for the application.
+//
+
+#include "BobiFluidCollab.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
